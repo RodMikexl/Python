00be0940f41cedce47b3e7acaf7590066ff6fee0
WLMike <a1071108631@gmail.com>
2023-02-07 10:39:22 +1100

    add method

--- pythonProject/Demo2/Demo2.py
+++ pythonProject/Demo2/Demo2.py
@@ -34,3 +34,37 @@ while i<length:
     i = i+1
 '''
 
+#增加 【append】
+'''
+namelist = ["Mike","Mkiex","Rock"]
+
+print("---增加前，名单列表的数据---")
+for name in namelist:
+    print(name)
+
+nametemp = input("请输入添加学生的名字:")
+namelist.append(nametemp)  #在末尾增加一个元素
+
+print("---增加后，名单列表的数据---")
+for name in namelist:
+    print(name)
+'''
+'''
+a = [1,2]
+b = [3,4]
+a.append(b) #将列表当作一个元素，加入列表a中
+print(a)
+
+a.extend(b) #将b列表的每个元素，追加到a列表中
+print(a)
+'''
+
+'''
+#增： [insert]
+a = [0,1,2]
+a.insert(1,3)   #第一个变量表示下标，第二个表示元素（对象）
+print(a)      #输出为：[0, 3, 1, 2] 指定下表位置插入元素
+'''
+
+#删减的方法
+
